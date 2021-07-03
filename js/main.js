2c2e66da24c98ce8d46483d2ff8ce0d1842c12e7
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-07-03 09:54:19 -0300

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 const menuMobile = document.getElementById('menu-mobile')
+const links = document.querySelectorAll('.link')
 
 menuMobile.addEventListener('click', () => {
     menuMobile.classList.toggle('header__menu--active')
     document.getElementsByClassName('header__ul')[0].classList.toggle('header__ul--active')
+
+    if(document.getElementsByClassName('header__ul')[0].classList.contains('header__ul--active')) {
+        document.body.style.overflowY = 'hidden'
+    } else {
+        document.body.style.overflowY = 'initial'
+    }
+})
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        menuMobile.classList.remove('header__menu--active')
+        document.getElementsByClassName('header__ul')[0].classList.remove('header__ul--active')
+        document.body.style.overflowY = 'initial'
+    })
 })
